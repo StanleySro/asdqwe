07f52bdf8bde3203c9285db0c14261b6b3994ba8
idoe <idoe@localhost.localdomain>
2020-01-21 00:13:59 +0100

    feature: working api

--- api/reservation.js
+++ api/reservation.js
@@ -1,20 +1,20 @@
 const Reservation = require("../models/Reservation");
+const User = require("../models/User");
 
 function put(req, res) {
-
-  User.findByIdAndUpdate(author,
-    {
-      $push: { reservations: reservation._id }
-    }, userError => {
-      if (userError) { console.error(userError); errors.user = "couldn't update user reservation"; res.status(404).json({ errors }); }
-      if (err) { console.error(err); errors.reservation = "couldn't create reservation"; res.status(404).json({ errors }); }
-      console.log("reservation = ", reservation); res.json(reservation);
-    });
-
   const { houseId, rentingPersonId, reservationStart, reservationEnd, details, startOfAccomodation, endOfAccomodation } = req.body;
   const newReservation = new Reservation(req.body);
-  newReservation.save().then(result => {
-    res.status(200).json(result)
+  newReservation.save().then(reservation => {
+    //res.status(200).json(reservation);
+    User.findByIdAndUpdate(rentingPersonId,
+      { $push: { reservations: reservation._id } }, userError => {
+        if (userError) {
+          console.error(userError);
+          errors.user = "couldn't update user reservation";
+          res.status(404).json({ errors });
+        }
+        res.status(200).json(reservation);
+    });
   }).catch(err => res.status(500).json({ error: err }));
 }
 
